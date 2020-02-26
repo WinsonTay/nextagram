ead8a052fb9d524f0f992843b4fbc9ecb7c7f812
WinsonTay <weeshen90@gmail.com>
2020-02-26 11:57:20 +0800

    added password and username for User class

--- start.py
+++ start.py
@@ -1,7 +1,16 @@
 from app import app
+from flask import Flask, render_template, url_for, request ,redirect
 import instagram_api
 import instagram_web
 
-# @approute('')
+@app.route('/')
+def users():
+    return render_template('home.html')
+
+@app.route('/new')
+def newuser():
+    return render_template('newuser.html')
+
+
 if __name__ == '__main__':
     app.run()
