1340b6ea838aca2140fcb81ab273754487bfba4d
WinsonTay <weeshen90@gmail.com>
2020-02-26 14:40:24 +0800

    "Added Hashing password using werkzeug security method"

--- start.py
+++ start.py
@@ -1,9 +1,12 @@
+from flask_wtf.csrf import CSRFProtect
+from werkzeug.security import generate_password_hash
 from app import app
 from flask import Flask, render_template, url_for, request ,redirect , flash
 from models import user 
 import instagram_api
 import instagram_web
 
+csrf = CSRFProtect(app)
 @app.route('/')
 def users():
     return render_template('home.html')
@@ -18,9 +21,10 @@ def createnew():
     name = request.form.get("name")
     password = request.form.get("password")
     username = request.form.get("username")
+    hash_password = generate_password_hash(password)
     email = request.form.get("email")
 
-    u = user.User(name = name, password=password, username =username, email = email)
+    u = user.User(name = name, password=hash_password, username =username, email = email)
     if u.save():
          flash("User successfully created.")
          return redirect(url_for('user_signup'))
