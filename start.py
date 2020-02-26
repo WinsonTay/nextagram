aa0a69ae5c952afe3eae234e5a7e39ac7684208a
WinsonTay <weeshen90@gmail.com>
2020-02-26 12:16:22 +0800

    Added Sign Up Page and form

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import Flask, render_template, url_for, request ,redirect
+from flask import Flask, render_template, url_for, request ,redirect , flash
+from models import user 
 import instagram_api
 import instagram_web
 
@@ -7,10 +8,26 @@ import instagram_web
 def users():
     return render_template('home.html')
 
-@app.route('/new')
-def newuser():
-    return render_template('newuser.html')
+@app.route('/sign_up')
+def user_signup():
+    return render_template('sign_up.html')
 
+
+@app.route('/new' ,methods = ["POST"])
+def createnew():
+    name = request.form("")
+    password = request.form("")
+    username = request.form("")
+
+
+    # u = user.User(name = name, password=password, username =username)
+    # if u.save():
+    #     flash("User successfully created.")
+    #     return redirect(url_for('user_signup'))
+    # else:
+    #     return render_template('/sign_up')
+    print(name,password,username)
+    return redirect(url_for('user_signup'))
 
 if __name__ == '__main__':
     app.run()
